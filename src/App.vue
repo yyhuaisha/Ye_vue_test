359cdd41af9a1f53dd851b934f4f4d87fb982fce
‘yyhuaisha <2433273227@qq.com>
2017-09-29 09:27:02 +0800

    vue脚手架 && demo

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p v-once></p>
+    <button @click="world='people'">改变world值</button>
+  </div>
+</template>
+
+<script>
+import myComponent from './component/myComponent.vue'
+import VueRouter from "vue-router"
+import VueResource from 'vue-resource'
+export default {
+  data () {
+    return {
+      world: 'World'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/component/myComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="myComponent">
+    <h1>I'm a title, I'm not a biaoti！</h1>
+    <p>{{youAre}}</p>
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default{
+    data() {
+      return {
+        youAre: `"你是大傻逼吗？" ， "是的，你是！"`
+      }
+    } 
+  }
+</script>
